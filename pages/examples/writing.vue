dd7ffc6da33f801619110d7744fe2c39d9187778
Kenni Bawden <kenni.bawden@gmail.com>
2020-10-06 12:12:37 +1100

    Created nuxtjs project

---
+++ pages/examples/writing.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Systems are in your <em>books</em>
+      </h1>
+      <h2 class="instructions">
+        Create a 4 letter word
+      </h2>
+      <h3 class="subtitle">
+        Writing system
+      </h3>
+      <div class="links">
+        <Button type="primary" target="_blank" rel="noopener noreferrer" to="examples/writing">
+          Next
+          <Icon type="ios-arrow-forward" />
+        </Button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="main-title">
+        Systems are everywhere 
+      </h1>
+      <h2 class="main-subtitle">
+        An introduction to systems thinking
+      </h2>
+      <div class="links">
+        <Button type="primary" target="_blank" rel="noopener noreferrer" to="examples/writing">
+          Next
+          <Icon type="ios-arrow-forward" />
+        </Button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
