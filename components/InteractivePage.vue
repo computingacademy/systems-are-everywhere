2658849c1fd7639e8bc36d30a69b2d12bc22c31c
Kenni Bawden <kenni.bawden@gmail.com>
2020-11-24 12:37:01 +1030

    Added instructions transitions and completion message

--- components/InteractivePage.vue
+++ components/InteractivePage.vue
@@ -2,7 +2,14 @@
   <div class="slide">
     <div class="content">
       <h1 class="title" v-html="title"></h1>
-      <h2 class="instructions" v-html="instruction"></h2>
+      <transition name="swap-instructions" v-for="(instruction, index) in instructions_">
+        <h2 class="instructions" v-html="instruction" v-if="index == step"></h2>
+      </transition>
+      <transition name="swap-instructions">
+        <h2 class="instructions" v-if="step >= instructions_.length">
+          <em>Task complete!</em>
+        </h2>
+      </transition>
       <slot></slot>
       <h3 class="subtitle" v-html="subtitle"></h3>
     </div>
@@ -16,6 +23,7 @@ export default {
   data() {
     return {
       step: 0,
+      instructions_: typeof this.instructions == 'string' ? [this.instructions] : this.instructions || [],
       completed: false,
     }
   },
@@ -23,17 +31,30 @@ export default {
     let vm = this
     this.$on('complete', function() {
       this.step++
-      if (this.step == this.instructions.length)
+      if (this.step == this.instructions_.length)
         vm.completed = true
     })
   },
-  computed: {
-    instruction() {
-      if (typeof this.instructions == 'string')
-        return this.instructions
-      else
-        return this.instructions[Math.min(this.step, this.instructions.length-1)]
-    },
-  }
 }
 </script>
+
+<style>
+.swap-instructions-enter-active {
+  display: inline-block;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: clip;
+  transition: width 4s;
+  width: 100%;
+}
+.swap-instructions-enter {
+  display: inline-block;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: clip;
+  width: 0px;
+}
+.swap-instructions-leave-to, .swap-instructions-leave-active {
+  display: none;
+}
+</style>
